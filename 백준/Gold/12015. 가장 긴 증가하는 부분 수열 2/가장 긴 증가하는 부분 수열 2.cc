1861eb83952d11da530b7136dc3361a3da13293e
Jiho Kim <131659584+RiMe-13@users.noreply.github.com>
2025-03-21 13:32:29 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 144 ms, Memory: 12200 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.cc
@@ -1,74 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-typedef pair<int, int> pii;
-
-int first_idx = 1;
-vector<pii> v;
+int n;
 int a[1000001];
-int dp[2100001]; // dp[i] : a[i]를 마지막 원소로 하는 LIS의 길이, 근데 인덱스 트리를 곁들인..
+vector<int> ans;
 
-int get_max(int a, int b) {
-	int left = first_idx + a, right = first_idx + b;
-	int res = 0;
+int binarySearch(int x) {
+    int left = 0, right = ans.size() - 1;
+    
+    while (left < right) {
+        int mid = (left + right) / 2;
 
-	while (left <= right) {
-		if (left % 2 == 1) {
-			res = max(res, dp[left]);
-			left++;
-		}
-		if (right % 2 == 0) {
-			res = max(res, dp[right]);
-			right--;
-		}
-		left /= 2;
-		right /= 2;
-	}
+        if (ans[mid] >= x) right = mid;
+        else left = mid + 1;
+    }
 
-	return res;
-}
-
-void update(int i, int val) {
-	int node = first_idx + i;
-	dp[node] = val;
-	while (node > 0) {
-		node /= 2;
-		dp[node] = max(dp[node * 2], dp[node * 2 + 1]);
-	}
+    return right;
 }
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-	int n;
-	cin >> n;
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    ans.push_back(a[0]);
 
-	while (first_idx < n) first_idx *= 2;
-	first_idx--;
+    for (int i = 1; i < n; i++) {
+        if (a[i] > ans.back()) ans.push_back(a[i]);
+        else {
+            int idx = binarySearch(a[i]);
+            ans[idx] = a[i];
+        }
+    }
+    cout << ans.size() << "\n";
 
-	for (int i = 1; i <= n; i++) {
-		cin >> a[i];
-		v.push_back({ a[i], -i });
-	}
-
-	sort(v.begin(), v.end());
-
-	for (pii x : v) {
-		int idx = x.second * (-1);
-		int val = get_max(1, idx - 1) + 1;
-		update(idx, val);
-	}
-	
-	int node = 1;
-	while (node <= first_idx) {
-		if (dp[node] == dp[node * 2]) node *= 2;
-		else node = node * 2 + 1;
-	}
-	cout << dp[1] << "\n";
-
-	return 0;
+    return 0;
 }
