4f65f0197dbe712644d9dcfb4984bdc05acfa340
Jiho Kim <131659584+RiMe-13@users.noreply.github.com>
2025-04-18 14:35:45 +0900

    [Gold III] Title: 파일 합치기, Time: 88 ms, Memory: 3004 KB -BaekjoonHub

--- 백준/Gold/11066. 파일 합치기/파일 합치기.cc
+++ 백준/Gold/11066. 파일 합치기/파일 합치기.cc
@@ -24,14 +24,11 @@ int main() {
             for (int i = 1; i <= k - l + 1; i++) {
                 int j = i + l - 1;
                 dp[i][j] = INF;
-                for (int k = i; k <= j - 1; k++) {
-                    int q = dp[i][k] + dp[k + 1][j] + sum[j] - sum[i - 1];
-                    if (q < dp[i][j]) dp[i][j] = q;
-                }
+                for (int k = i; k <= j - 1; k++) dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + sum[j] - sum[i - 1]);
             }
         }
         cout << dp[1][k] << "\n";
     }
-    
+
     return 0;
 }
