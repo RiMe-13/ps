87958ecc2ba68f6220531f931c6387b8f1dc59c4
Jiho Kim <131659584+RiMe-13@users.noreply.github.com>
2025-06-23 18:26:39 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 76 ms, Memory: 10932 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, m, r, cnt = 1;
+int visited[100001];
+vector<int> adj[100001];
+
+void dfs(int x) {
+    visited[x] = cnt++;
+
+    for (int v : adj[x]) {
+        if (!visited[v]) dfs(v);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m >> r;
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    for (int i = 1; i <= n; i++) sort(adj[i].begin(), adj[i].end(), greater<>());
+
+    dfs(r);
+    
+    for (int i = 1; i <= n; i++) cout << visited[i] << "\n";
+
+    return 0;
+}
