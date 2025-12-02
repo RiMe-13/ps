83bcec2e34f015179c97c29c68903486c3bf75c2
Jiho Kim <131659584+RiMe-13@users.noreply.github.com>
2025-12-02 12:41:46 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> a >> b;
+    cout << a + b << "\n";
+
+    return 0;
+}
