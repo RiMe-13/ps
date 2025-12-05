4634792a287d01ac2c9370666eafb3618f2ff112
Jiho Kim <131659584+RiMe-13@users.noreply.github.com>
2025-12-05 18:16:05 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+string str;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> str;
+    cout << str << "??!\n";
+
+    return 0;
+}
