b832032ba5472a369b101515cb77f25d495a25bf
Jiho Kim <131659584+RiMe-13@users.noreply.github.com>
2025-12-06 15:00:46 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int y;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> y;
+    cout << y - 543 << "\n";
+
+    return 0;
+}
