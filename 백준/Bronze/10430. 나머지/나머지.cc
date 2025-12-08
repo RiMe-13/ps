18f9dcc1a2a15293d4bf861ed41b5d9f654b13db
Jiho Kim <131659584+RiMe-13@users.noreply.github.com>
2025-12-08 19:37:38 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> a >> b >> c;
+    cout << (a + b) % c << "\n";
+    cout << ((a % c) + (b % c)) % c << "\n";
+    cout << (a * b) % c << "\n";
+    cout << ((a % c) * (b % c)) % c << "\n";
+
+    return 0;
+}
