2b6009d774626b012f849296a194fa823d01b69d
Jiho Kim <131659584+RiMe-13@users.noreply.github.com>
2025-12-10 12:45:42 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+long long a, b, c;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> a >> b >> c;
+    cout << a + b + c << "\n";
+
+    return 0;
+}
