4f9d0fded9f051c95e290a9bb89569c15a077ccf
Jiho Kim <131659584+RiMe-13@users.noreply.github.com>
2026-03-26 12:59:45 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int adj[101][101];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) cin >> adj[i][j];
+    }
+
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (adj[i][k] && adj[k][j]) adj[i][j] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) cout << adj[i][j] << " ";
+        cout << "\n";
+    }
+
+    return 0;
+}
