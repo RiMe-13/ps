b3c6076e458ea3d9112559f96ab59348bee9f4dd
Jiho Kim <131659584+RiMe-13@users.noreply.github.com>
2026-04-23 12:14:59 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 32 ms, Memory: 7800 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
@@ -1,36 +1,13 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int n;
-vector<int> v;
-int score[1000001];
-bool card[1000001];
-
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        int tmp;
-        cin >> tmp;
-        v.push_back(tmp);
-        card[tmp] = true;
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = v[i] * 2; j < 1000001; j += v[i]) {
-            if (card[j]) {
-                score[v[i]]++;
-                score[j]--;
-            }
-        }
-    }
-
-    for (int i = 0; i < n; i++) cout << score[v[i]] << " ";
-    cout << "\n";
-
-    return 0;
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+<hr><center>nginx/1.22.1</center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
